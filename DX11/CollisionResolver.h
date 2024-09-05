6b3d1eeaf5ac85640d5f49ee3d00aa091156f9d9
PinTrees <59812031+PinTrees@users.noreply.github.com>
2024-09-05 20:10:11 +0900

    update collision resolver

--- DX11/CollisionResolver.h
+++ DX11/CollisionResolver.h
@@ -3,8 +3,9 @@
 class CollisionResolver
 {
 private:
-	float penetrationTolerance = 0.0005f;
-	float closingSpeedTolerance = 0.005f;
+	float penetrationTolerance = 0.0005f; 
+	//float closingSpeedTolerance = 0.005f;
+	float closingSpeedTolerance = 0.015f;
 
 public:
 	void ResolveCollision(std::vector<Contact*>& contacts, float deltaTime);
