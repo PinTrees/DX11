792af2886154e90f2c988203a438b775e6802c29
PinTrees <59812031+PinTrees@users.noreply.github.com>
2024-09-27 16:10:44 +0900

    add (de)serialize float4, string, wstring

--- DX11/JsonUtility.h
+++ DX11/JsonUtility.h
@@ -17,9 +17,24 @@ Quaternion SafeGetQuaternion(const json& j, const std::string& key, const Quater
 #define DE_SERIALIZE_VECTOR3(JSON, VALUE) VALUE = SafeGetVector3(JSON, #VALUE, Vector3::Zero);
 #define DE_SERIALIZE_VECTOR3_D(JSON, VALUE, Default) VALUE = SafeGetVector3(JSON, #VALUE, Default); 
 
-#define SERIALIZE_INT(JSON, VALUE, VALUE_NAME) JSON[VALUE_NAME] = VALUE;
 
+#define SERIALIZE_INT(JSON, VALUE, VALUE_NAME) JSON[VALUE_NAME] = VALUE;
 #define DE_SERIALIZE_INT(JSON, VALUE, VALUE_NAME) if (JSON.contains(VALUE_NAME) && JSON.at(VALUE_NAME).is_number_integer()) \
 													VALUE = j.at(VALUE_NAME).get<int>();									\
 												  else VALUE = 0;															\
 
+
+#define SERIALIZE_STRING(JSON, VALUE, VALUE_NAME) JSON[VALUE_NAME] = VALUE;
+#define SERIALIZE_WSTRING(JSON, VALUE, VALUE_NAME) JSON[VALUE_NAME] = wstring_to_string(VALUE);
+
+#define SERIALIZE_FLOAT4(JSON, VALUE, VALUE_NAME) JSON[VALUE_NAME] = { VALUE.x, VALUE.y,VALUE.z, VALUE.w };
+#define DE_SERIALIZE_FLOAT4(JSON, VALUE, VALUE_NAME) if (JSON.contains(VALUE_NAME)) {                                                   \
+                                                         auto __val = JSON.at(VALUE_NAME).get<std::vector<float>>();                    \
+                                                         if (__val.size() == 4) {                                                       \
+                                                             VALUE = XMFLOAT4{ __val[0], __val[1], __val[2], __val[3] };                \
+                                                         } else {                                                                       \
+                                                             VALUE = XMFLOAT4{ 0, 0, 0, 0 };                                            \
+                                                         }                                                                              \
+                                                     } else {                                                                           \
+                                                         VALUE = XMFLOAT4{ 0, 0, 0, 0 };                                                \
+                                                     }							
--- DX11/UMaterial.cpp
+++ DX11/UMaterial.cpp
@@ -225,14 +225,11 @@ void from_json(const json& j, UMaterial& m)
 	m.m_BaseMapPath = string_to_wstring(j.at("BaseMapPath").get<std::string>());
 	m.m_NormalMapPath = string_to_wstring(j.at("NormalMapPath").get<std::string>());
 	m.m_ResourcePath = j.at("ResourcePath").get<std::string>();
-	auto ambient = j.at("Ambient").get<std::vector<float>>();
-	m.Ambient = XMFLOAT4{ ambient[0], ambient[1], ambient[2], ambient[3] };
-	auto diffuse = j.at("Diffuse").get<std::vector<float>>();
-	m.Diffuse = XMFLOAT4{ diffuse[0], diffuse[1], diffuse[2], diffuse[3] };
-	auto specular = j.at("Specular").get<std::vector<float>>();
-	m.Specular = XMFLOAT4{ specular[0], specular[1], specular[2], specular[3] };
-	auto reflect = j.at("Reflect").get<std::vector<float>>();
-	m.Reflect = XMFLOAT4{ reflect[0], reflect[1], reflect[2], reflect[3] };
+	
+	DE_SERIALIZE_FLOAT4(j, m.Ambient, "Ambient");
+	DE_SERIALIZE_FLOAT4(j, m.Diffuse, "Diffuse");
+	DE_SERIALIZE_FLOAT4(j, m.Specular, "Specular");
+	DE_SERIALIZE_FLOAT4(j, m.Reflect, "Reflect");
 
 	DE_SERIALIZE_INT(j, m.m_shaderSetting.UseTexture, "UseTexture");
 	DE_SERIALIZE_INT(j, m.m_shaderSetting.AlphaClip, "AlphaClip");
@@ -245,16 +242,16 @@ void from_json(const json& j, UMaterial& m)
 
 void to_json(json& j, const UMaterial& m)
 {
-	j = json
-	{
-		{ "BaseMapPath", wstring_to_string(m.m_BaseMapPath) },
-		{ "NormalMapPath", wstring_to_string(m.m_NormalMapPath) },
-		{ "ResourcePath", m.m_ResourcePath },
-		{ "Ambient", { m.Ambient.x, m.Ambient.y, m.Ambient.z, m.Ambient.w } },
-		{ "Diffuse", { m.Diffuse.x, m.Diffuse.y, m.Diffuse.z, m.Diffuse.w } },
-		{ "Specular", { m.Specular.x, m.Specular.y, m.Specular.z, m.Specular.w } },
-		{ "Reflect", { m.Reflect.x, m.Reflect.y, m.Reflect.z, m.Reflect.w } },
-	};
+	j = json {};
+
+	SERIALIZE_WSTRING(j, m.m_BaseMapPath, "BaseMapPath");
+	SERIALIZE_WSTRING(j, m.m_NormalMapPath, "NormalMapPath");
+	SERIALIZE_STRING(j, m.m_ResourcePath, "ResourcePath"); 
+
+	SERIALIZE_FLOAT4(j, m.Ambient, "Ambient");
+	SERIALIZE_FLOAT4(j, m.Diffuse, "Diffuse");
+	SERIALIZE_FLOAT4(j, m.Specular, "Specular");
+	SERIALIZE_FLOAT4(j, m.Reflect, "Reflect");
 
 	// ShaderSetting
 	SERIALIZE_INT(j, m.m_shaderSetting.UseTexture, "UseTexture");
